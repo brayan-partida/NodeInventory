d2f26fa6e2917072b37a362fcea58a766cd5c4ee
brayan-partida <brayan.ramirez.partida@gmail.com>
2021-05-25 19:49:47 -0700

    pruba heroku

--- src/routes/departamentRoutes.js
+++ src/routes/departamentRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const departamentController = require("../controllers/departamentControllers");
 const controller = require("../controllers/usersControllers");
 
-router.get("https://serviciosocialtec.herokuapp.com//departamentall", departamentController.list);
+router.get("https://serviciosocialtec.herokuapp.com/departamentall", departamentController.list);
 router.post("/departamentadd", departamentController.add);
 router.put("/departamentupdate/:idDepartament", departamentController.update);
 
