2e4d0a3562d8849883e4a8e67f5e6330c4a8a080
Brayan Enrique Ramirez Partida <eramirez@ensenada.gob.mx>
2021-09-28 09:39:02 -0700

    primer commit para prueba de jenkins

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,7 +2,7 @@ const controller = {};
 const user = require("../query/usersQuery");
 const pool = require("../database/database");
 const User = require("../models/Users");
-const { request } = require("express");
+const {request} = require("express");
 const userRoutes = require("../routes/usersRoutes");
 const jwt = require("jsonwebtoken");
 const config = require("../../config");
@@ -15,9 +15,9 @@ const verifyToken = require("../Utils/verifyToken");
  * @returns
  */
 controller.list = async (req, res) => {
-  
-  const query = await pool.query(user.selectFrom());
-  res.json(query);
+
+    const query = await pool.query(user.selectFrom());
+    res.json(query);
 };
 
 /**
@@ -27,16 +27,15 @@ controller.list = async (req, res) => {
  * @param {*} res
  */
 controller.listNick = async (req, res) => {
-  const { nick } = req.params;
-  const query = await pool.query(user.selectFromNick(nick));
-  if (query.length === 0) {
-    res.json({
-      error: "no se encontro el nick  del usuario",
-    });
-  } else {
-    res.json(query);
-  }
-}; //ANCHOR select Users where=====>
+    const {nick} = req.params;
+    const query = await pool.query(user.selectFromNick(nick));
+    if (query.length === 0) {
+        res.json({error: "no se encontro el nick  del usuario"});
+    } else {
+        res.json(query);
+    }
+};
+// ANCHOR select Users where=====>
 
 /**
  * Ingresa los datos para poder registrar un nuevo usuario
@@ -45,35 +44,29 @@ controller.listNick = async (req, res) => {
  * @param {*} res
  */
 controller.add = async (req, res) => {
-  const { id, nombre, nick, pass, type, usersDepartament } = req.body;
-  userObjet = new User(id, nombre, nick, pass, type, usersDepartament);
-  const buscarNick = await pool.query(user.selectFromNick(nick));
-  if (buscarNick.length === 0) {
-    var query = await pool.query(
-      user.addNick(
-        userObjet.getNombre(),
-        userObjet.getNick(),
-        userObjet.getPass(),
-        userObjet.getType(),
-        userObjet.getUsersDepartament()
-      )
-    );
-    //NOTE puede manejar de dos a tres parametros de los cual es login , encryp, tiempo de espera
-    const token = jwt.sign({ nick: nick, pass: pass }, config.secret, {
-      expiresIn: 86400,
-    });
-    res.json({
-      auth: true,
-      token,
-    });
-    //res.redirect("/api/users/allUsers");
-  } //valida si se encuentra el usuario en la base de datos
-  else {
-    res.json({
-      error: "este nick ya se encuentra registrado",
-    });
-  }
-};
+    const {
+        id,
+        nombre,
+        nick,
+        pass,
+        type,
+        usersDepartament
+    } = req.body;
+    userObjet = new User(id, nombre, nick, pass, type, usersDepartament);
+    const buscarNick = await pool.query(user.selectFromNick(nick));
+    if (buscarNick.length === 0) {
+        var query = await pool.query(user.addNick(userObjet.getNombre(), userObjet.getNick(), userObjet.getPass(), userObjet.getType(), userObjet.getUsersDepartament()));
+        // NOTE puede manejar de dos a tres parametros de los cual es login , encryp, tiempo de espera
+        const token = jwt.sign({
+            nick: nick,
+            pass: pass
+        }, config.secret, {expiresIn: 86400});
+        res.json({auth: true, token});
+        // res.redirect("/api/users/allUsers");
+    } // valida si se encuentra el usuario en la base de datos else {
+        res.json({error: "este nick ya se encuentra registrado"});
+    }
+
 /**
  * actualiza el nick para poder
  *
@@ -81,12 +74,10 @@ controller.add = async (req, res) => {
  * @param {json} res
  */
 controller.update = async (req, res) => {
-  const { nick } = req.params;
-  const { nombre, pass, type, usersDepartament } = req.body;
-  const query = await pool.query(
-    user.updateNick(nombre, pass, type, usersDepartament, nick)
-  );
-  res.redirect("/api/users/allUsers");
-}; //ANCHOR UPDATE Users=======>
+    const {nick} = req.params;
+    const {nombre, pass, type, usersDepartament} = req.body;
+    const query = await pool.query(user.updateNick(nombre, pass, type, usersDepartament, nick));
+    res.redirect("/api/users/allUsers");
+}; // ANCHOR UPDATE Users=======>
 
 module.exports = controller;
